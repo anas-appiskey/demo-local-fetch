2295f13db661da5134659693b7b0b91d62788295
anasali2193 <anasali2193@gmail.com>
2020-07-09 15:25:03 +0500

    initial commit

---
+++ index-fetch-data.js
@@ -0,0 +1,30 @@
+async function getData(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    console.log("response = ",response);
+
+    const data = await response.json();
+    console.log("data " ,data)
+}
+
+async function postData(){
+    const response = await  fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'my titly',
+            body : 'bar',
+            userId : 1
+        }),
+        headers : {
+            "Content-type" : "application/json; charset= UTF-8"
+        }
+    });
+
+    console.log("response = ",response);
+
+    const data = await response.json();
+    console.log("data " ,data)
+}
+
+// getData();
+
+postData();
